bc913b776c11f0e6459cb06daa80c2af8844d5dc
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-07-01 17:10:24 +0700

    add export modules

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
-export { BankingProviderProps } from './src/contexts/index';
-export { BankContext } from './src/contexts/bank-context';
-export { WalletContext } from './src/contexts/wallet-context';
+export {
+  WalletContext,
+  TransactionContext,
+  BankContext,
+  BankComponentProvider,
+} from './src/contexts';
 export { BankingService } from './src/services/banking-service';
+export { default as AccountComponent } from './src/components/account';
+export { default as BankLoginComponent } from './src/components/bank-login';
+export { default as ConsentComponent } from './src/components/consent';
+export { default as LinkBankComponent } from './src/components/link-bank';
+export { default as SelectBankComponent } from './src/components/select-bank';
+export { default as TransactionComponent } from './src/components/transaction';
+export * from './src/types';
---
+++ src/contexts/index.ts
@@ -0,0 +1,4 @@
+export { BankContext } from './bank-context';
+export { default as BankComponentProvider } from './provider';
+export { TransactionContext } from './transaction-context';
+export { WalletContext } from './wallet-context';
