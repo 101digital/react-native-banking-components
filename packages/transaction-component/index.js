55750c5795cbfb8ea6e141a29d8a6dd100706c07
TuyenNguyenVan <tuyen@101digital.io>
2021-09-14 18:14:59 +0700

    update

--- packages/transaction-component/index.js
+++ packages/transaction-component/index.js
@@ -1,4 +1,4 @@
 export { TransactionContext } from './src/context/transaction-context';
-export { TransactionProvider } from './src/context/transaction-provider';
+export { default as TransactionProvider } from './src/context/transaction-provider';
 export { TransactionService } from './src/service/transaction-service';
 export { default as TransactionComponent } from './src/index';
