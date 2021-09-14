0d07d28cefa52dcde6084c76ae05fa628c138468
TuyenNguyenVan <tuyen@101digital.io>
2021-09-14 19:15:47 +0700

    update

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -2,30 +2,22 @@ import {
   defaultsDeep as _defaultsDeep,
   isEmpty as _isEmpty,
   uniqBy as _uniqBy,
-  ValueIteratee,
   List,
   groupBy as _groupBy,
-  Dictionary,
   chain as _chain,
   orderBy as _orderBy,
   CollectionChain,
   FunctionChain,
-  ListIteratee,
-  Many,
   StringNullableChain,
 } from 'lodash';
 
-export const defaultsDeep = (object: any, ...source: any[]) =>
-  _defaultsDeep(object, source);
+export const defaultsDeep = _defaultsDeep;
 
-export const isEmpty = (object?: Object) => _isEmpty(object);
+export const isEmpty = _isEmpty;
 
 export const isNotEmpty = (object?: Object) => !_isEmpty(object);
 
-export const uniqBy = <T>(
-  array: List<T> | null | undefined,
-  iteratee: ValueIteratee<T>
-): T[] => _uniqBy(array, iteratee);
+export const uniqBy = _uniqBy;
 
 export const getUrlParameter = (url: string, name: string) => {
   name = name.replace(/\\[[]/, '\\[').replace(/[\]]/, '\\]');
@@ -36,10 +28,7 @@ export const getUrlParameter = (url: string, name: string) => {
     : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-export const groupBy = <T>(
-  collection: List<T> | null | undefined,
-  iteratee?: ValueIteratee<T>
-): Dictionary<T[]> => _groupBy(collection, iteratee);
+export const groupBy = _groupBy;
 
 export const chain1 = (value: string | null | undefined): StringNullableChain =>
   _chain(value);
@@ -52,8 +41,4 @@ export const chain3 = <T = any>(
   value: List<T> | null | undefined
 ): CollectionChain<T> => _chain(value);
 
-export const orderBy = <T>(
-  collection: List<T> | null | undefined,
-  iteratees?: Many<ListIteratee<T>>,
-  orders?: Many<boolean | 'asc' | 'desc'>
-): T[] => _orderBy(collection, iteratees, orders);
+export const orderBy = _orderBy;
