6753a512638fa7d823bfaf22657c6249a79f362b
TuyenNguyenVan <tuyen@101digital.io>
2021-10-11 17:51:26 +0700

    update version:

--- packages/cashflow-component/index.d.ts
+++ packages/cashflow-component/index.d.ts
@@ -1,3 +1,4 @@
 export { CashflowContext } from './src/context/cashflow-context';
 export { default as CashflowProvider } from './src/context/cashflow-provider';
 export { FinancialService } from './src/services/financial-service';
+export { default as CashflowComponent } from './src/index';
