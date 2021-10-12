441bb5338306221eadc565f8c0ff4e577aa1f910
TuyenNguyenVan <tuyen@101digital.io>
2021-10-12 10:28:30 +0700

    update bank model

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -34,6 +34,10 @@ export interface Bank {
   id: string;
   imageUrl: string;
   isLinked: boolean;
+  isInternalVirtualBank: boolean;
+  countryCode: string;
+  accreditedDataRecipientId: string;
+  customerFacing: boolean;
 }
 
 export interface Paging {
