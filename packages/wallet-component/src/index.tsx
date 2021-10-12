7109d9d872865e7261ea2a77aec5f80cd27e7503
TuyenNguyenVan <tuyen@101digital.io>
2021-10-12 17:16:25 +0700

    update

--- packages/wallet-component/src/index.tsx
+++ packages/wallet-component/src/index.tsx
@@ -53,13 +53,12 @@ const WalletComponent = forwardRef((props: WalletComponentProps, ref) => {
   const [isShowUnlink, setShowUnlink] = useState(false);
   const [isShowPrimary, setShowPrimary] = useState(false);
   const [walletWithBanners, setWalletWithBanners] = useState<Wallet[]>([]);
-  const [isSelectedPrimary, setSelectedPrimary] = useState(false);
+  // const [isSelectedPrimary, setSelectedPrimary] = useState(false);
 
   useEffect(() => {
     if (!linkedWallet) {
-      setTimeout(() => {
-        setSelectedPrimary(true);
-      }, 300);
+      fetchWallets();
+      clearLinkedWallet();
     }
   }, [linkedWallet]);
 
@@ -103,62 +102,62 @@ const WalletComponent = forwardRef((props: WalletComponentProps, ref) => {
     setShowActionSheet(false);
   };
 
-  const renderSuccessModal = () => {
-    return (
-      <AlertModal
-        style={LinkAccountSuccessModal?.alertStyle}
-        isVisible={
-          !isLinkingWallet && linkedWallet !== undefined && !ConfirmSetPrimaryModal?.props?.disable
-        }
-        title={
-          LinkAccountSuccessModal?.props?.title ??
-          i18n?.t('wallet_component.lbl_link_successfully') ??
-          'Account Added Succefully'
-        }
-        confirmTitle={
-          LinkAccountSuccessModal?.props?.confirmButonLabel ??
-          i18n?.t('wallet_component.btn_continue') ??
-          'Continue'
-        }
-        onClose={() => {
-          clearLinkedWallet();
-          fetchWallets();
-        }}
-        isFullWidth={LinkAccountSuccessModal?.props?.isFullWidth}
-        isShowClose={LinkAccountSuccessModal?.props?.isShowClose}
-        message={
-          LinkAccountSuccessModal?.props?.message ??
-          i18n?.t('wallet_component.msg_link_successfully') ??
-          'Your bank account is successfully linked to your profile.'
-        }
-        onConfirmed={() => {
-          if (isSelectedPrimary) {
-            const wallet = linkedWallet;
-            if (wallet) {
-              setPrimaryWallet(wallet.walletId);
-            }
-          } else {
-            clearLinkedWallet();
-            fetchWallets();
-          }
-        }}
-      >
-        {LinkAccountSuccessModal?.components?.renderSetPrimary?.(isSelectedPrimary, () => {
-          setSelectedPrimary(!isSelectedPrimary);
-        }) ?? (
-          <SetPrimaryComponent
-            i18n={i18n}
-            isSelected={isSelectedPrimary}
-            disabled={isEmpty(wallets)}
-            style={LinkAccountSuccessModal?.style}
-            onPressed={() => setSelectedPrimary(!isSelectedPrimary)}
-            {...LinkAccountSuccessModal?.props}
-            {...LinkAccountSuccessModal?.components}
-          />
-        )}
-      </AlertModal>
-    );
-  };
+  // const renderSuccessModal = () => {
+  //   return (
+  //     <AlertModal
+  //       style={LinkAccountSuccessModal?.alertStyle}
+  //       isVisible={
+  //         !isLinkingWallet && linkedWallet !== undefined && !ConfirmSetPrimaryModal?.props?.disable
+  //       }
+  //       title={
+  //         LinkAccountSuccessModal?.props?.title ??
+  //         i18n?.t('wallet_component.lbl_link_successfully') ??
+  //         'Account Added Succefully'
+  //       }
+  //       confirmTitle={
+  //         LinkAccountSuccessModal?.props?.confirmButonLabel ??
+  //         i18n?.t('wallet_component.btn_continue') ??
+  //         'Continue'
+  //       }
+  //       onClose={() => {
+  //         clearLinkedWallet();
+  //         fetchWallets();
+  //       }}
+  //       isFullWidth={LinkAccountSuccessModal?.props?.isFullWidth}
+  //       isShowClose={LinkAccountSuccessModal?.props?.isShowClose}
+  //       message={
+  //         LinkAccountSuccessModal?.props?.message ??
+  //         i18n?.t('wallet_component.msg_link_successfully') ??
+  //         'Your bank account is successfully linked to your profile.'
+  //       }
+  //       onConfirmed={() => {
+  //         if (isSelectedPrimary) {
+  //           const wallet = linkedWallet;
+  //           if (wallet) {
+  //             setPrimaryWallet(wallet.walletId);
+  //           }
+  //         } else {
+  //           clearLinkedWallet();
+  //           fetchWallets();
+  //         }
+  //       }}
+  //     >
+  //       {LinkAccountSuccessModal?.components?.renderSetPrimary?.(isSelectedPrimary, () => {
+  //         setSelectedPrimary(!isSelectedPrimary);
+  //       }) ?? (
+  //         <SetPrimaryComponent
+  //           i18n={i18n}
+  //           isSelected={isSelectedPrimary}
+  //           disabled={isEmpty(wallets)}
+  //           style={LinkAccountSuccessModal?.style}
+  //           onPressed={() => setSelectedPrimary(!isSelectedPrimary)}
+  //           {...LinkAccountSuccessModal?.props}
+  //           {...LinkAccountSuccessModal?.components}
+  //         />
+  //       )}
+  //     </AlertModal>
+  //   );
+  // };
 
   if (isEmpty(wallets)) {
     if (isLoadingWallets) {
@@ -180,7 +179,7 @@ const WalletComponent = forwardRef((props: WalletComponentProps, ref) => {
           {...EmptyWallet?.props}
           {...EmptyWallet?.components}
         />
-        {renderSuccessModal()}
+        {/* {renderSuccessModal()} */}
       </>
     );
   }
@@ -323,7 +322,7 @@ const WalletComponent = forwardRef((props: WalletComponentProps, ref) => {
           setSelectedWallet(undefined);
         }}
       />
-      {renderSuccessModal()}
+      {/* {renderSuccessModal()} */}
     </>
   );
 });
