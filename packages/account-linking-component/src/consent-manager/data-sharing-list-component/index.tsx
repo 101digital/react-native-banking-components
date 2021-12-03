f9e18119f8cfe76223070c7d536ab6426790d96d
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2021-12-03 10:44:28 +0700

    update

--- packages/account-linking-component/src/consent-manager/data-sharing-list-component/index.tsx
+++ packages/account-linking-component/src/consent-manager/data-sharing-list-component/index.tsx
@@ -4,12 +4,15 @@ import { ActivityIndicator, FlatList, Text, TouchableOpacity, View } from 'react
 import { isEmpty } from '@banking-component/core';
 import { Image, ThemeContext } from 'react-native-theme-component';
 import { ArrowRightIcon, images } from '../../assets/images';
+import useMergeStyles from './styles';
+import { DataSharingListComponentProps, DataSharingListComponentStyles } from '../types';
 
-const DataSharingListComponent = () => {
+const DataSharingListComponent = (props: DataSharingListComponentProps) => {
   const { getAccountConsents, accountConsents, isLoadingAccountConsents } = useContext(
     AccountLinkingContext
   );
-  const { colors, fonts } = useContext(ThemeContext);
+  const { colors } = useContext(ThemeContext);
+  const styles: DataSharingListComponentStyles = useMergeStyles(props.style);
 
   useEffect(() => {
     getAccountConsents();
@@ -18,87 +21,34 @@ const DataSharingListComponent = () => {
   if (isEmpty(accountConsents)) {
     if (isLoadingAccountConsents) {
       return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+        <View style={styles.loadingContainerStyle}>
           <ActivityIndicator color={colors.primaryColor} />
         </View>
       );
     }
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>Empty consents</Text>
-      </View>
-    );
+    return <View />;
   }
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.containerStyle}>
       <FlatList
         keyExtractor={(item) => item.accountConsentId}
         data={accountConsents}
+        style={styles.listContainerStyle}
+        ItemSeparatorComponent={() => <View style={styles.listDividerStyle} />}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity
-              activeOpacity={0.8}
-              style={{
-                backgroundColor: 'white',
-                flexDirection: 'row',
-                alignItems: 'center',
-                paddingVertical: 15,
-                paddingHorizontal: 18,
-              }}
-            >
-              <View
-                style={{
-                  width: 55,
-                  height: 50,
-                  borderWidth: 1,
-                  borderColor: '#F5F5F5',
-                  borderRadius: 5,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
+            <TouchableOpacity activeOpacity={0.8} style={styles.itemContainerStyle}>
+              <View style={styles.imageContainerStyle}>
                 <Image
                   source={{ uri: item.aspspInfo.imageUrl }}
                   fallbackImage={images.bank}
-                  style={{
-                    width: 32,
-                    height: 32,
-                  }}
+                  style={styles.imageStyle}
                 />
               </View>
-              <View style={{ flex: 1, marginLeft: 15 }}>
-                <Text
-                  style={{
-                    fontSize: 14,
-                    fontFamily: fonts.regular,
-                    color: '#244065',
-                  }}
-                >
-                  {item.aspspInfo.name}
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 12,
-                    fontFamily: fonts.regular,
-                    color: '#828282',
-                    marginTop: 5,
-                  }}
-                >
-                  {item.createdAt}
-                </Text>
+              <View style={styles.itemContentContainerStyle}>
+                <Text style={styles.itemTitleStyle}>{item.aspspInfo.name}</Text>
+                <Text style={styles.itemPeriodStyle}>{item.createdAt}</Text>
               </View>
               <ArrowRightIcon size={13} color='rgba(13, 32, 80, 0.5)' />
             </TouchableOpacity>
--- packages/account-linking-component/src/consent-manager/types.tsx
+++ packages/account-linking-component/src/consent-manager/types.tsx
@@ -1,14 +1,23 @@
 import { AccountConsent } from '@banking-component/core';
-import { ImageStyle, StyleProp, ViewStyle } from 'react-native';
+import { ImageStyle, StyleProp, TextStyle, ViewStyle } from 'react-native';
 
-export type DataSharingListComponentProps = {};
+export type DataSharingListComponentProps = {
+  style?: DataSharingListComponentStyles;
+};
 
 export type DataSharingListComponentStyles = {
   containerStyle?: StyleProp<ViewStyle>;
+  loadingContainerStyle?: StyleProp<ViewStyle>;
+  sectionContainerStyle?: StyleProp<ViewStyle>;
+  sectionTitleStyle?: StyleProp<TextStyle>;
+  listContainerStyle?: StyleProp<ViewStyle>;
+  listDividerStyle?: StyleProp<ViewStyle>;
   itemContainerStyle?: StyleProp<ViewStyle>;
   imageContainerStyle?: StyleProp<ViewStyle>;
   imageStyle?: StyleProp<ImageStyle>;
   itemContentContainerStyle?: StyleProp<ViewStyle>;
+  itemTitleStyle?: StyleProp<TextStyle>;
+  itemPeriodStyle?: StyleProp<TextStyle>;
 };
 
 export type DataSharingDetailComponentProps = {
